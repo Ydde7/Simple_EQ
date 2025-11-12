2baeadae7b6f33b9f99141987a500e05d4371a34
Teddy <thehofamily2020@gmail.com>
2025-11-12 11:08:49 -0800

    3. Setup processes and context; Roadblock with VST plugin (41:00)

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -95,6 +95,22 @@ void Simple_EQAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBl
 {
     // Use this method as the place to do any pre-playback
     // initialisation that you need..
+
+    // Filters must be prepared before usage : To do this, pass a "ProcessSpec" object
+        // Chains will pass this singular context to each link in the chain.
+    juce::dsp::ProcessSpec spec; 
+
+    spec.maximumBlockSize = samplesPerBlock; // Maximum number of samples
+
+    spec.numChannels = 1; // the number of channels is 1 since we are dealing with 2 MonoChains.
+
+    spec.sampleRate = sampleRate; // Provide the SampleRate
+
+    // Passing the specs to the MonoChannel objects. 
+        // Context is passed in the processblock
+    leftChain.prepare(spec);
+    rightChain.prepare(spec);
+
 }
 
 void Simple_EQAudioProcessor::releaseResources()
@@ -129,6 +145,9 @@ bool Simple_EQAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts
 }
 #endif
 
+// Called by the host and given a buffer of **any number of channels**
+    // For our stereo system, we need the right and left channels from the buffer
+    // typically: Left = 0; Right = 1;
 void Simple_EQAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
 {
     juce::ScopedNoDenormals noDenormals;
@@ -144,18 +163,23 @@ void Simple_EQAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, ju
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
+    // Create a block from the buffer
+    juce::dsp::AudioBlock<float> block(buffer);
 
-        // ..do something to the data...
-    }
+    // Extract the channels from the block
+    auto leftBlock = block.getSingleChannelBlock(0);
+    auto rightBlock = block.getSingleChannelBlock(1);
+
+    // Create the context based on blocks
+    juce::dsp::ProcessContextReplacing<float> leftContext(leftBlock);
+    juce::dsp::ProcessContextReplacing<float> rightContext(rightBlock);
+
+    // Pass the context to the chains
+    leftChain.process(leftContext);
+    rightChain.process(rightContext);
+
+    // At this point, the coeffiecients are not set up. Do that to get audio
+        // This requires a VST plugin to load and play audio files. Create this from the Juce tutorial page (Audio)
 }
 
 //==============================================================================
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -62,7 +62,30 @@ public:
     // using "*this" audio processor | no UndoManager | "Parameters" is the valueTreeType | We create a function to return the layout
     juce::AudioProcessorValueTreeState apvts{ *this, nullptr, "Parameters", createParameterLayout() };
 
+
 private:
+    // Alias for dsp namespace mess
+        // Each has a response of 12 DB/OCT
+        // To get 48 DB/OCT, we need 4 Filters linked by a chain.
+            // the chain passes a single context to each filter automatically.
+        
+    // Filter, CutFilter, etc are the aliases 
+
+    using Filter = juce::dsp::IIR::Filter<float>;
+
+    using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
+
+    // Goal: We need 2 monoChains for stereo Processing
+
+    //Defining a chain for the mono path
+    using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+
+    // This declares two MonoChains which are "juce::dsp::ProcessorChain<>"under an alias.
+    MonoChain leftChain, rightChain; 
+
+
+
+
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Simple_EQAudioProcessor)
 };
